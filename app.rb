e584b50b27016bc9f0314fefbb22f69b54279768
Вячеслав Николаев <pleskachev777@gmail.com>
2019-03-23 23:23:18 +0300

    execute simple query

--- app.rb
+++ app.rb
@@ -1,3 +1,7 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new 't.sqlite' 
+db = SQLite3::Database.new 't.sqlite'
+
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Moto K-750', 777)"
+
+db.close 
